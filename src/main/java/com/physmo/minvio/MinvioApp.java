ceeb99b4136bd2b4ae60b84e6dadc2efd695b67a
Nick Donnelly <nickd3000@hotmail.com>
2021-07-05 23:54:13 +0100

    added new minvio version

---
+++ src/main/java/com/physmo/minvio/MinvioApp.java
@@ -0,0 +1,12 @@
+package com.physmo.minvio;
+
+public abstract class MinvioApp {
+
+    public abstract void init();
+    public abstract void update(BasicDisplay bd, double delta);
+
+    public void start(int width, int height, int fps) {
+
+    }
+
+}
